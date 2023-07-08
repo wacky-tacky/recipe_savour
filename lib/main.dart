ad61df7cb5f1a423ef1beebb0707e7a69bd80f83
wacky-tacky <vedikagarg242@gmail.com>
2023-07-09 01:06:58 +0530

    5 screens
    - Bottom navigation bar added
    - Side drawer added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
+import './screens/meal_detail_screen.dart';
 import 'screens/category_meals_screen.dart';
 import 'screens/categories_screen.dart';
 
@@ -42,7 +43,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
+import './favourites_screen.dart';
+import './categories_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavouriteScreen(),
+      'title': 'Favourites',
+    },
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //  ADDING TABS ON TOP OF THE SCREEN
+
+    // return DefaultTabController(
+    //   length: 2,
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text('Meals'),
+    //       bottom: TabBar(
+    //         tabs: [
+    //           Tab(
+    //             icon: Icon(
+    //               Icons.food_bank_rounded,
+    //             ),
+    //             text: 'Categories',
+    //           ),
+    //           Tab(
+    //             icon: Icon(Icons.favorite),
+    //             text: 'Favourites',
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //     body: TabBarView(
+    //       children: [
+    //         CategoriesScreen(),
+    //         FavouriteScreen(),
+    //       ],
+    //     ),
+    //   ),
+    // );
+
+    //  ADDING TABS AT THE BOTTOM OF THE SCREEN
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _pages[_selectedPageIndex]['title'] as String,
+        ),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.food_bank_rounded),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favourites',
+          ),
+        ],
+      ),
+    );
+  }
+}
